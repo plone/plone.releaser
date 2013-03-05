093b899a8a4f8868f3d1f3d447cb9203e72b6aa5
Eric Steele <EricSteele47@gmail.com>
2013-03-05 08:09:04 -0500

    Increase depth of package clone, so that we have a better chance of finding the last tag.

--- esteele/manager/manage.py
+++ esteele/manager/manage.py
@@ -170,7 +170,7 @@ def checkPackageForUpdates(package_name):
             if source.protocol == 'git':
                 tmpdir = mkdtemp()
                 # print "Reading %s branch of %s for changes since %s..." % (source.branch, package_name, version)
-                repo = git.Repo.clone_from(source.url, tmpdir, branch=source.branch, depth=20)
+                repo = git.Repo.clone_from(source.url, tmpdir, branch=source.branch, depth=100)
 
                 g = git.Git(tmpdir)
                 try:
