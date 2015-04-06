e22f09cb648ec2eeca3ff252d95852b9c41433f4
Gil Forcada <gforcada@gnome.org>
2015-04-06 22:09:37 +0200

    Formatting improvements
    - put all setup arguments on a new line
    - keep long description generation outside of setup()
    - sort classifiers
    - space entry points so future additions/removals are less noisy
    - add commas at the end of all lists (again for less noisy diffs)

--- setup.py
+++ setup.py
@@ -5,46 +5,55 @@ from setuptools import setup
 
 version = '1.3.dev0'
 
-setup(name='plone.releaser',
-      version=version,
-      description='Plone release management utilities',
-      long_description=open('README.rst').read() + '\n' +
-      open('CHANGES.rst').read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-          'Programming Language :: Python',
-          'Framework :: Plone',
-          'Programming Language :: Python :: 2.7',
+long_description = '{0}\n{1}'.format(
+    open('README.rst').read(),
+    open('CHANGES.rst').read()
+)
+
+setup(
+    name='plone.releaser',
+    version=version,
+    description='Plone release management utilities',
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
+      'Framework :: Plone',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 2.7',
+    ],
+    keywords='plone release',
+    author='Eric Steele',
+    author_email='eric@esteele.net',
+    url='https://github.com/plone/plone.releaser',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'setuptools',
+      'argh',
+      'gitpython>=0.3',
+      'configparser',
+      'argcomplete',
+      'progress',
+      'PyGithub',
+      'keyring',
+      'zest.releaser',
+      'docutils',
+      'launchpadlib',
+    ],
+    entry_points={
+      'console_scripts': [
+          'manage = plone.releaser.manage:manage',
       ],
-      keywords='plone release',
-      author='Eric Steele',
-      author_email='eric@esteele.net',
-      url='https://github.com/plone/plone.releaser',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'argh',
-          'gitpython>=0.3',
-          'configparser',
-          'argcomplete',
-          'progress',
-          'PyGithub',
-          'keyring',
-          'zest.releaser',
-          'docutils',
-          'launchpadlib'
+      'zest.releaser.prereleaser.middle': [
+          'show_changelog=plone.releaser.release:show_changelog_entries',
+          'check_pypi=plone.releaser.release:check_pypi_access',
       ],
-      entry_points={
-          'console_scripts': [
-              'manage = plone.releaser.manage:manage'],
-          'zest.releaser.prereleaser.middle': [
-              'show_changelog=plone.releaser.release:show_changelog_entries',
-              'check_pypi=plone.releaser.release:check_pypi_access'],
-          'zest.releaser.releaser.after': [
-              'update_core=plone.releaser.release:update_core']},
-      )
+      'zest.releaser.releaser.after': [
+          'update_core=plone.releaser.release:update_core',
+      ]
+    },
+)
