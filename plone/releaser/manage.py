cfaf2cd07b1fef4fa83e33513638e26d3dfcf017
Gil Forcada <gforcada@gnome.org>
2015-04-06 22:12:51 +0200

    One element per line

--- plone/releaser/manage.py
+++ plone/releaser/manage.py
@@ -22,28 +22,34 @@ import keyring
 import os
 
 
-THIRD_PARTY_PACKAGES = ['Zope2',
-                        'ZODB3',
-                        'txtfilter',
-                        'Products.CMFActionIcons',
-                        'Products.CMFCalendar',
-                        'Products.CMFCore',
-                        'Products.CMFDefault',
-                        'Products.CMFTopic',
-                        'Products.CMFUid',
-                        'Products.DCWorkflow',
-                        'Products.GenericSetup',
-                        'Products.GroupUserFolder',
-                        'Products.PluggableAuthService',
-                        'Products.PluginRegistry',
-                        'Products.ZCatalog']
+THIRD_PARTY_PACKAGES = [
+    'Zope2',
+    'ZODB3',
+    'txtfilter',
+    'Products.CMFActionIcons',
+    'Products.CMFCalendar',
+    'Products.CMFCore',
+    'Products.CMFDefault',
+    'Products.CMFTopic',
+    'Products.CMFUid',
+    'Products.DCWorkflow',
+    'Products.GenericSetup',
+    'Products.GroupUserFolder',
+    'Products.PluggableAuthService',
+    'Products.PluginRegistry',
+    'Products.ZCatalog',
+]
 
-IGNORED_PACKAGES = ['plone.releaser']
+IGNORED_PACKAGES = [
+    'plone.releaser',
+]
 
-ALWAYS_CHECKED_OUT = ['Plone',
-                      'Products.CMFPlone',
-                      'plone.app.upgrade',
-                      'plone.app.locales']
+ALWAYS_CHECKED_OUT = [
+    'Plone',
+    'Products.CMFPlone',
+    'plone.app.upgrade',
+    'plone.app.locales',
+]
 
 
 # TODO
