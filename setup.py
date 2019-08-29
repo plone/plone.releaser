03b6c7d35839c6f324cfa8d680aecc38b2ea5c67
Maurits van Rees <maurits@vanrees.org>
2019-08-29 22:58:33 +0200

    Added classifiers for Python 3.6 and 3.7 to setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -21,6 +21,8 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     keywords="plone release",
     author="Eric Steele",
