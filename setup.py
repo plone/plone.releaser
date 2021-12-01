6522ec4d9f01e2b24c1b68e4cd5706854084590a
Maurits van Rees <maurits@vanrees.org>
2021-12-01 01:38:47 +0100

    Back to development: 1.8.6
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "1.8.5"
+version = "1.8.6.dev0"
 
 long_description = "{0}\n{1}".format(
     open("README.rst").read(), open("CHANGES.rst").read()
