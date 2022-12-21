fc80077bffc3ff2d974ff99eb7572bf83debf7b2
Maurits van Rees <maurits@vanrees.org>
2022-12-21 16:26:10 +0100

    Add requires_python.
    [ci skip]

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     keywords="plone release",
     author="Eric Steele",
@@ -36,6 +37,7 @@ setup(
     namespace_packages=["plone"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
     install_requires=[
         "setuptools",
         "argh",
