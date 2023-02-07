dcb96db7dc95be69b1664d72d4f1f4fbf6e96f3b
Maurits van Rees <maurits@vanrees.org>
2023-02-07 23:13:02 +0100

    Bumped version for breaking release.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "1.8.9.dev0"
+version = "2.0.0.dev0"
 
 long_description = "{}\n{}".format(
     open("README.rst").read(), open("CHANGES.rst").read()
